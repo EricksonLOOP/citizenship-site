dad2c4a6c710d460a73f0b8a8c5a969213cd8cee
Erickson Dias <103853894+EricksonLOOP@users.noreply.github.com>
2025-01-08 12:00:12 -0300

    created/ not-found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function Error() {
+    return (
+        <div className="bg-white h-[100vh] w-[100vw] flex flex-col items-center justify-center">
+            <h1 className="text-cyan-600 font-black text-[3rem]">Unity <b className="text-blue-600">Citizenship</b></h1>
+            <span className="font-semibold text-red-500">Page Not Found!</span>
+        </div>
+    )
+} 
